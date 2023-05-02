7d9bcceafd3abe46851da922ffe1b72dc400754b
Alaixs <63883086+Alaixs@users.noreply.github.com>
2023-05-02 12:54:02 +0000

    ratio ivan

---
+++ h/globales.h
@@ -0,0 +1,15 @@
+/**
+ ******************************************************************************************************************
+ * @file    globales.h
+ * @author  IUT Informatique La Rochelle
+ * @version v1.1
+ * @date    2021
+ * @brief   d�clarations des variables globales
+ ******************************************************************************************************************/
+
+#ifndef __GLOBALES_H
+#define __GLOBALES_H
+
+volatile int lAbscisse;
+
+#endif
