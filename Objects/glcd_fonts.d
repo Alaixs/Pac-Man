11bd4b979fec3a6c3faedf03a4a87a3e40923247
JibiLine <92849748+JibiLine@users.noreply.github.com>
2023-05-02 15:43:14 +0200

    Update glcd_fonts.d

--- Objects/glcd_fonts.d
+++ Objects/glcd_fonts.d
@@ -1,4 +1,1 @@
-.\objects\glcd_fonts.o: src\GLCD_Fonts.c
-.\objects\glcd_fonts.o: .\h\Board_GLCD.h
-.\objects\glcd_fonts.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\glcd_fonts.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\objects\glcd_fonts.o: src\GLCD_fonts.c
