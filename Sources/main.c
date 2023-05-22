381f5ea066496cb2369a82549419662101128047
alexis piekarz <alexis.piekarz@etudiant.univ-lr.fr>
2023-05-22 09:26:57 +0200

    add menu

--- Sources/main.c
+++ Sources/main.c
@@ -7,86 +7,119 @@
   * @modification	
   * @brief   
   ******************************************************************************************************************/
-
 #include "GLCD.h"
 #include "GLCD_Config.h"
 #include "ext_globales.h"
 #include "homescreen.h"
 #include "boule.h"
 #include "timers.h"
+#include "menu.h"
+#include "GPIO.h"
 
 /*----------------------------------------------------------------------------
-  Main Program
- *----------------------------------------------------------------------------*/
-
+Function prototypes
+----------------------------------------------------------------------------*/
 void Enable_GPIO(void);
 void Init_GPIO(void);
 
-// Initialisation des variables externes
-int xBoule = 0;
-int yBoule = 0;
+/*----------------------------------------------------------------------------
+Global variables
+----------------------------------------------------------------------------*/
+int xBoule = 200;
+int yBoule = 200;
 int menu = 1;
 int oldXBoule;
 int oldYBoule;
+char dir;
 
+/*----------------------------------------------------------------------------
+Main program
+----------------------------------------------------------------------------*/
+int main(void) {
+Enable_GPIO(); // Enable GPIO ports
+Init_GPIO(); // Initialize GPIO ports
 
-int main (void){
-	Enable_GPIO(); // Mise en service des ports
-	Init_GPIO();  // Initialisation des Ports
-	
-	//initalisation du LCD
-	initGLCD();
-	GLCD_DrawBitmap(170,200,
-									LARGEUR_BOULE, HAUTEUR_BOULE,
-									(const unsigned char *)bmpBoule);
-	
-	
-	//lancement du timer
-	cfgTimer1();
-	
-	
-  //affichage de l'�cran d'accueil
+// Initialize the LCD
+initGLCD();
 
-	GLCD_SetBackgroundColor(GLCD_COLOR_WHITE);
-	GLCD_SetFont(&GLCD_Font_16x24);
-	GLCD_DrawChar(125,100,'P');
-	GLCD_SetBackgroundColor(GLCD_COLOR_WHITE);
-	GLCD_DrawChar(138,100,'A');
-	GLCD_SetBackgroundColor(GLCD_COLOR_WHITE);
-	GLCD_DrawChar(155,100,'C');
-	GLCD_SetBackgroundColor(GLCD_COLOR_WHITE);
-	GLCD_DrawChar(170,100,'M');
-	GLCD_SetBackgroundColor(GLCD_COLOR_WHITE);
-	GLCD_DrawChar(185,100,'A');
-	GLCD_SetBackgroundColor(GLCD_COLOR_WHITE);
-	GLCD_DrawChar(200,100,'N');
-	GLCD_SetBackgroundColor(GLCD_COLOR_WHITE);
+// Display the home screen menu
+displayMenu(JOUER);
+while (!refreshMenu());
 
-  //affichage de l'�cran d'accueil
-	while (1)// boucle de jeu
-	{
-		while(!menu) // boucle d'affichage en jeu
-		{
-			GLCD_DrawBitmap(xBoule,yBoule,
-											LARGEUR_BOULE, HAUTEUR_BOULE,
-											(const unsigned char *)bmpBoule);
-			GLCD_DrawBitmap(oldXBoule,oldYBoule,
-											LARGEUR_BOULE, HAUTEUR_BOULE,
-											(const unsigned char *)bmpEraseBoule);
-		}
-	}
+// Configure and start the timer
+//cfgTimer1();
+	
+clearScreenGLCD();
+
+while (1) {
+
+	if (joytickRightPressed())
+  {
+		GLCD_DrawBitmap(xBoule, yBoule,
+		LARGEUR_BOULE, HAUTEUR_BOULE,
+		(const unsigned char*)bmpEraseBoule);
+    xBoule++;
+		GLCD_DrawBitmap(xBoule, yBoule,
+		LARGEUR_BOULE, HAUTEUR_BOULE,
+		(const unsigned char*)bmpBoule);
+  }
+  if (joytickLeftPressed())
+  {
+		GLCD_DrawBitmap(xBoule, yBoule,
+		LARGEUR_BOULE, HAUTEUR_BOULE,
+		(const unsigned char*)bmpEraseBoule);
+    xBoule--;
+		GLCD_DrawBitmap(xBoule, yBoule,
+		LARGEUR_BOULE, HAUTEUR_BOULE,
+		(const unsigned char*)bmpBoule);
+  }
+  if (joytickUpPressed())
+  {
+		GLCD_DrawBitmap(xBoule, yBoule,
+		LARGEUR_BOULE, HAUTEUR_BOULE,
+		(const unsigned char*)bmpEraseBoule);
+    yBoule--;
+		GLCD_DrawBitmap(xBoule, yBoule,
+		LARGEUR_BOULE, HAUTEUR_BOULE,
+		(const unsigned char*)bmpBoule);
+  }
+  if (joytickDownPressed())
+  {
+		GLCD_DrawBitmap(xBoule, yBoule,
+		LARGEUR_BOULE, HAUTEUR_BOULE,
+		(const unsigned char*)bmpEraseBoule);
+    yBoule++;
+		GLCD_DrawBitmap(xBoule, yBoule,
+		LARGEUR_BOULE, HAUTEUR_BOULE,
+		(const unsigned char*)bmpBoule);
+  }
+
 }
-               
-void Enable_GPIO(void){
-	RCC->APB2ENR |= (1 << 3); // Enable GPIOB clock
-	RCC->APB2ENR |= (1 << 8); // Enable GPIOG clock
-	RCC->APB2ENR |= (1 << 5); // Enable GPIOD clock
 }
 
-void Init_GPIO(void){
-	GPIOB->CRH = 0x33333333; // Mode=0b11 (50MHz) et CNF=0b00 (Push-Pull) pour PB8 � PB15 (Leds 8 � 15)
-	GPIOG->CRH |= 0x44400000; // Mode=0b00 (Input Mode) et CNF=0b01 (Floating Input) pour  PG15, PG14 et PG13
-	GPIOG->CRL |= 0x40000000; // Mode=0b00 (Input Mode) et CNF=0b01 (Floating Input) pour  PG7
-	GPIOD->CRL |= 0x00004000; // Mode=0b00 (Input Mode) et CNF=0b01 (Floating Input) pour  PD3
-	
+
+
+
+
+
+
+
+
+/*----------------------------------------------------------------------------
+Enable GPIO ports
+----------------------------------------------------------------------------*/
+void Enable_GPIO(void) {
+RCC->APB2ENR |= (1 << 3); // Enable GPIOB clock
+RCC->APB2ENR |= (1 << 8); // Enable GPIOG clock
+RCC->APB2ENR |= (1 << 5); // Enable GPIOD clock
+}
+
+/*----------------------------------------------------------------------------
+Initialize GPIO ports
+----------------------------------------------------------------------------*/
+void Init_GPIO(void) {
+GPIOB->CRH = 0x33333333; // Set mode=0b11 (50MHz) and CNF=0b00 (Push-Pull) for PB8 to PB15 (Leds 8 to 15)
+GPIOG->CRH |= 0x44400000; // Set mode=0b00 (Input Mode) and CNF=0b01 (Floating Input) for PG15, PG14, and PG13
+GPIOG->CRL |= 0x40000000; // Set mode=0b00 (Input Mode) and CNF=0b01 (Floating Input) for PG7
+GPIOD->CRL |= 0x00004000; // Set mode=0b00 (Input Mode) and CNF=0b01 (Floating Input) for PD3
 }
---
+++ Sources/menu.c
@@ -0,0 +1,69 @@
+/**
+  ******************************************************************************************************************
+  * @file    menu.c
+  * @author  IUT Informatique La Rochelle
+  * @version v1.0
+  * @date    2023
+  * @modification	
+  * @brief   Gestion du menu
+  ******************************************************************************************************************/
+
+#include "GLCD_Config.h"
+#include "Board_GLCD.h"
+#include "GLCD.h"
+
+#include "menu.h"
+#include "ext_globales.h"
+#include "GPIO.h"
+
+#include "stdio.h"
+
+//affichage de l'écran d'accueil
+void displayMenu(MenuSelected menuSelected)
+{	
+	clearScreenGLCD();	
+	displayLogo();
+	
+	if(menuSelected==JOUER)
+	{ 
+		GLCD_SetForegroundColor(GLCD_COLOR_RED);
+		GLCD_DrawString  (100, 100,"Jouer");
+		GLCD_SetForegroundColor(GLCD_COLOR_WHITE);
+		GLCD_DrawString (100, 140, "Quitter");
+	}
+	else if(menuSelected==QUITTER)
+	{ 
+		GLCD_SetForegroundColor(GLCD_COLOR_WHITE);
+		GLCD_DrawString (100, 100,"Jouer");
+		GLCD_SetForegroundColor(GLCD_COLOR_RED);
+		GLCD_DrawString (100, 140,"Quitter");
+	}
+}
+
+int refreshMenu()
+{
+	static MenuSelected menuSelected = JOUER;
+	
+	if(joytickUpPressed() && menuSelected == QUITTER)
+	{
+		menuSelected = JOUER;
+		displayMenu(menuSelected);
+	}
+	if(joytickDownPressed()&& menuSelected == JOUER) 
+	{
+		menuSelected = QUITTER;
+		displayMenu(menuSelected);
+	}
+	
+	return joytickSelectPressed() && menuSelected==JOUER;
+}
+
+
+void displayLogo()
+{
+	GLCD_SetBackgroundColor(GLCD_COLOR_BLACK);
+	GLCD_SetForegroundColor(GLCD_COLOR_YELLOW);
+	GLCD_SetFont(&GLCD_Font_16x24);
+	GLCD_DrawString  (90, 5,"PAC-MAN");
+}
+
